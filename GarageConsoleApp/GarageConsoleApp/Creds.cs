d2889b8c972f73ad90471b11105759304098ff1c
drost-dev <andrey.06.ezdakov@gmail.com>
2024-04-16 20:17:40 +0700

    creds defaults :)

---
+++ GarageConsoleApp/GarageConsoleApp/Creds.cs
@@ -0,0 +1,10 @@
+﻿namespace GarageConsoleApp;
+
+public class Creds
+{
+    static public string Host { get; } = "localhost";
+    static public string Port { get; } = "5432";
+    static public string Database { get; } = "garage_db";
+    static public string Username { get; } = "postgres";
+    static public string Password { get; } = "qwerty";
+}
